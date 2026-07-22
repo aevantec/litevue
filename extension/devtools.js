f96fe7c64b272648b25be746c80004ac80313b27
abiacarl <abiacarl@gmail.com>
2026-07-22 11:12:54 +0800

    Add extension icons

--- extension/devtools.js
+++ extension/devtools.js
@@ -1,3 +1,3 @@
 /* global chrome */
 // registers the "litevue" tab in the browser devtools
-chrome.devtools.panels.create('litevue', '', 'panel.html');
+chrome.devtools.panels.create('litevue', 'icons/icon-16.png', 'panel.html');
